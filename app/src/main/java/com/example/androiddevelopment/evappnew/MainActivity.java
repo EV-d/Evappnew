90b361806a57dc33f0bbcd23ad4c2cf44e9aae82
androiddevelopment <anwarsalim07@gmail.com>
2015-02-04 19:43:25 +0400

    waleed added button

--- app/src/main/java/com/example/androiddevelopment/evappnew/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopment/evappnew/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,9 +16,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button forwaleed = (Button) findViewById(R.id.btnwaleed);
+        forwaleed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"weew i did it",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
