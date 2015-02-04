2e9c748f2231fa3cf17bcd5218cf597c2ec74378
anwar salim <anwarsalim07@gmail.com>
2015-02-04 15:17:22 +0400

    Update MainActivity.java
    tese

--- app/src/main/java/com/example/androiddevelopment/evappnew/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopment/evappnew/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,7 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+        
     }
 
     @Override
